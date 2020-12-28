6db72c33b29ffec566f0d7ac18d888c24c29b89b
Ricardo Olarte <raop155@hotmail.com>
2020-12-28 18:37:33 -0500

    Add fetch API & Loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import Map from './components/Map';
+import Loader from './components/Loader';
 
 function App() {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+
+    console.log(eventData);
+  }, []);
+
+  useEffect(() => {
+    console.log('ESPERA EL ASYNC?');
+  }, []);
+
+  return <div>{!loading ? <Map evenData={eventData} /> : <Loader />}</div>;
 }
 
 export default App;
